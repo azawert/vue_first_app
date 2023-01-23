4ba87b309b2bb2235a8c6e536745276ce618c851
azawert <azawertik@gmail.com>
2023-01-23 19:43:48 +0300

    first vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <form>
+        <input type="text" class="input_field" placeholder="title"/>
+        <input type="text" class ="input_field" placeholder="description" />
+        <button @click="(e)=>{
+          e.preventDefault()
+          console.log(description,title)
+        }">Add post</button>
+      </form>
+    </div>
+    <div class="post" v-for="item in posts">
+      <div>
+        <strong>Title:</strong>
+        {{item.title}}
+      </div>
+      <div>
+        <strong>Description:</strong>
+        {{item.description}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts:[{title:'JavaScript',description:'Frontend language'},{title:'Python',description: 'BackEnd Language'}]
+    }
+  },
+  methods: {
+    addPost(event,data) {
+      event.preventDefault()
+      return [...this.posts,data]
+    }
   }
+
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid darkgreen;
+  margin-top: 15px;
+}
+.input_field {
+  margin-right: 10px;
+  border-radius: 6px;
+  margin-top: 10px;
+  width:100%;
+  padding: 10px;
+  border: 1px solid teal;
+
+}
+form {
+  display:flex;
+  flex-direction: column;
+}
+button {
+  padding: 5px;
+  margin-top: 5px;
+  width: 5%;
+  align-self: flex-end;
+  margin-right: 5px;
 }
 </style>
